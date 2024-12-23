f34063bb02ad2567757ad0be527bf9fce0f3eb19
Bicky Tamang <bickytamang101@gmail.com>
2024-12-23 14:02:33 +0545

    chore: updated the files constant

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -37,29 +37,36 @@ export const files = [
   {
     id: 1,
     name: "test.txt",
+    isDuplicate: true,
   },
   {
     id: 2,
     name: "yourname.jpg",
+    isDuplicate: false,
   },
   {
     id: 3,
     name: "test-data.pdf",
+    isDuplicate: true,
   },
   {
     id: 4,
     name: ".gitignore",
+    isDuplicate: true,
   },
   {
     id: 5,
     name: "config",
+    isDuplicate: false,
   },
   {
     id: 6,
     name: "types.tsx",
+    isDuplicate: false,
   },
   {
     id: 7,
     name: "data.ts",
+    isDuplicate: true,
   },
 ];
